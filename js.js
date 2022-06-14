10b21c08f460a9accab1ad3adb6852fdaf3312f6
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-06-14 09:11:47 -0300

    confirmar

---
+++ js.js
@@ -0,0 +1,48 @@
+// function isLeap(year){
+//     if(year % 4 === 0){
+//         if(year % 100 === 0){
+//             if(year % 400 === 0){
+//                 return "Leap Year";
+//             } else{
+//             return "Leap year"
+//             }
+//         } else{ 
+//             return "Leap year"
+//         }
+//     } else{
+//         return "Leap year"
+//     }
+// }
+
+
+// ============== FizzBuzz
+
+// var output = [1,2,3];
+// var count = 1;
+
+// function fizzBuzz(){
+//     if (count % 3 === 0 && count % 5 === 0){
+//         output.push("fizzBuzz")
+//     }
+//     else if (count % 3 === 0){
+//         output.push("Fizz")
+//     } 
+//     else if (count % 5 === 0){
+//         output.push("Buzz")
+//     } 
+//     else {
+//         output.push(count)
+//     }
+
+
+//     output.push(count);
+//     count++;
+
+//     console.log(output)
+// }
+
+
+
+
+
+
